590e9950ba80d33d3ec73f31a85cd435b64628a4
JaceksFridge <Japacz22@gmail.com>
2023-08-05 18:06:46 +0200

    yee

---
+++ test.py
@@ -0,0 +1,23 @@
+
+import os
+import openai
+from secret import OPEN_KEY
+
+
+
+key = OPEN_KEY
+
+
+openai.api_key = key
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{
+            "role": "user",
+            "content": "Write 10 Bullet Points why Java sucks."
+        }],
+    temperature=0,
+    max_tokens=1024
+)
+
+print(response['choices'][0]['message']['content'])
